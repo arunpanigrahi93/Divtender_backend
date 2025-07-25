12db3a779d5e3da62e7ae434d4053be10696f984
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2025-07-25 20:27:38 +0530

    multipule route handlers

--- src/app.js
+++ src/app.js
@@ -2,26 +2,25 @@ const express = require("express");
 
 const app = express();
 
-// This will only handle GET call to /user
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Arun", lastName: "Kumar" });
-});
+app.use(
+  "/user",
+  [
+    (req, res, next) => {
+      next();
 
-app.post("/user", (req, res) => {
-  res.send("Data saved succefully in DB");
-});
-app.patch("/user", (req, res) => {
-  res.send("Update data succefully");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Deleted the data successfully");
-});
-
-// This will match all the HTTP method API call to /test
-app.use("/test", (req, res) => {
-  res.send("test");
-});
+      // res.send("response 1");
+    },
+    (req, res, next) => {
+      next();
+    },
+  ],
+  (req, res) => {
+    res.send("Response 3");
+  },
+  (req, res) => {
+    res.send("Response 4");
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server running succesfully");
