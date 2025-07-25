0292cafd5d3097b59c974bd1f3abe3569c80f965
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2025-07-25 12:33:49 +0530

    explore Get,Post,Patch and Delete

--- src/app.js
+++ src/app.js
@@ -7,6 +7,17 @@ app.get("/user", (req, res) => {
   res.send({ firstName: "Arun", lastName: "Kumar" });
 });
 
+app.post("/user", (req, res) => {
+  res.send("Data saved succefully in DB");
+});
+app.patch("/user", (req, res) => {
+  res.send("Update data succefully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted the data successfully");
+});
+
 // This will match all the HTTP method API call to /test
 app.use("/test", (req, res) => {
   res.send("test");
