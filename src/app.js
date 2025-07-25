523d2a234e544b8263105bfa573f0845da4fe55f
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2025-07-25 12:13:45 +0530

    created server

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("hello from server");
+// This will only handle GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Arun", lastName: "Kumar" });
 });
-app.use("/", (req, res) => {
-  res.send("hello hello homepage");
+
+// This will match all the HTTP method API call to /test
+app.use("/test", (req, res) => {
+  res.send("test");
 });
 
 app.listen(3000, () => {
