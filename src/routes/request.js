11a3e89456636e1faaa88a005e5f903d71e02e39
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2025-08-12 09:01:21 +0530

    connection req sent

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,5 +1,34 @@
 const express = require("express");
+const userAuth = require("../middlewares/auth");
+const ConnectionRequest = require("../model/connectionRequestModel");
 
 const requestRoute = express.Router();
+
+requestRoute.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    const fromUserId = req.user._id;
+    const toUserId = req.params.toUserId;
+    const status = req.params.status;
+
+    const connectionRequest = new ConnectionRequest({
+      fromUserId,
+      toUserId,
+      status,
+    });
+
+    const data = await connectionRequest.save();
+
+    res.json({
+      message: "Connection request send successfully",
+      data,
+    });
+    try {
+    } catch (err) {
+      console.log("Error" + err.message);
+    }
+  }
+);
 
 module.exports = requestRoute;
