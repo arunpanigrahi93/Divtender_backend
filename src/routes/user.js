68265f5a139812caa03df63b26de750b3f543cbf
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2025-08-12 21:11:43 +0530

    get connections api

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const userAuth = require("../middlewares/auth");
 const { Connection } = require("mongoose");
-const connectionRequest = require("../model/connectionRequestModel");
+const ConnectionRequest = require("../model/connectionRequestModel");
 
 const userRouter = express.Router();
 
@@ -9,17 +9,51 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
 
-    const connectionRequests = await connectionRequest
-      .find({
-        toUserId: loggedInUser._id,
-        status: "interested",
-      })
-      .populate("fromUserId", ["firstName", "lastName"]);
+    const connectionRequests = await ConnectionRequest.find({
+      toUserId: loggedInUser._id,
+      status: "interested",
+    }).populate("fromUserId", [
+      "firstName",
+      "lastName",
+      "photoUrl",
+      "age",
+      "gender",
+      "about",
+      "skills",
+    ]);
 
     res.json({
       message: "Data fetched successfully",
       data: connectionRequests,
     });
+  } catch (err) {
+    res.status(400).send("ERROR :" + err.message);
+  }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    }).populate("fromUserId", [
+      "firstName",
+      "lastName",
+      "photoUrl",
+      "age",
+      "gender",
+      "about",
+      "skills",
+    ]);
+
+    //we need to extract fromuseriddata form all info thru map
+
+    const data = connectionRequests.map((row) => row.fromUserId);
+    res.json({ data });
   } catch (err) {
     res.status(400).send("ERROR :" + err.message);
   }
